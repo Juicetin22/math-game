28a22cd63cdab706a20318b2331a1072d609008c
Justin Huang <justin.2260@gmail.com>
2022-01-31 18:12:23 -0500

    add class Game and initialize method, along with testing initial game

---
+++ game.rb
@@ -0,0 +1,17 @@
+class Game
+  def initialize(player_1, player_2)
+    @player_1 = player_1
+    @player_2 = player_2
+    @players = [@player_1, @player_2]
+    @turn = 1
+    @limit = "0/3"
+  end
+
+  def play
+    puts "Time to play! It's #{@player_1} vs #{@player_2}!!!"
+
+    while ()
+
+
+  end
+end
