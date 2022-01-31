99336b6fd1813ccdb7aa86c2b2538f49898394ba
Justin Huang <justin.2260@gmail.com>
2022-01-31 18:12:58 -0500

    create main UI, separate classes from this file

---
+++ main.rb
@@ -0,0 +1,17 @@
+require './game'
+require './player'
+
+print "Please enter your name: "
+name_1 = gets.chomp
+
+print "Please enter your opponent's name: "
+name_2 = gets.chomp
+
+player_1 = Player.new(name_1)
+player_2 = Player.new(name_2)
+
+p player_1
+p player_2
+
+game = Game.new(player_1.name, player_2.name)
+game.play
