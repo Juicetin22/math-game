41a9a3067d4cdb5cfd3e9015235613e769a833d8
Justin Huang <justin.2260@gmail.com>
2022-01-31 23:16:54 -0500

    create method for single play(round) and to display the score

--- game.rb
+++ game.rb
@@ -1,17 +1,41 @@
 class Game
+  attr_reader :players
+
   def initialize(player_1, player_2)
-    @player_1 = player_1
-    @player_2 = player_2
-    @players = [@player_1, @player_2]
+    @players = [player_1, player_2].shuffle
     @turn = 1
     @limit = "0/3"
   end
 
+  def display_score
+    puts " -------------------------- "
+    puts "Current scores"
+    puts " -------------------------- "
+    players.each {|player| puts "#{player.name}: #{player.remaining}"}
+  end
+
   def play
-    puts "Time to play! It's #{@player_1} vs #{@player_2}!!!"
+    puts " -------------------------- "
+    puts "Time to play! It's #{players.first.name} vs #{players.last.name}!!!"
+    puts " -------------------------- "
 
-    while ()
+    puts "--------- NEW TURN ---------"
+    questioning_player = @players.first
+    answering_player = @players.last
 
+    numbers = questioning_player.question
 
+    solution = answering_player.answer(numbers.first, numbers.last)
+
+    if (solution == numbers.first + numbers.last) 
+      puts "#{questioning_player.name}: YES! You are correct"
+      answering_player.calculate_score(true)
+    else
+      puts "#{questioning_player.name}: Seriously? No!"
+      answering_player.calculate_score(false)
+    end
+    
+    display_score
+ 
   end
 end
