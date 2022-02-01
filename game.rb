29191cebb254ae1f3d2ab781fa0ad83e7072543f
Justin Huang <justin.2260@gmail.com>
2022-01-31 23:58:33 -0500

    add in a loop to continue the game; after reaching the limit, determine the loser and display final messages

--- game.rb
+++ game.rb
@@ -14,28 +14,53 @@ class Game
     players.each {|player| puts "#{player.name}: #{player.remaining}"}
   end
 
+  def game_over?
+    players.first.remaining == @limit || players.last.remaining == @limit
+  end
+
+  def new_turn
+    @turn += 1
+    players.rotate!
+  end
+
+  def winner
+    players.detect {|player| !player.lost?}
+  end
+
   def play
     puts " -------------------------- "
     puts "Time to play! It's #{players.first.name} vs #{players.last.name}!!!"
     puts " -------------------------- "
+    puts
 
-    puts "--------- NEW TURN ---------"
-    questioning_player = @players.first
-    answering_player = @players.last
+    until (game_over?) do
 
-    numbers = questioning_player.question
+      puts "--------- TURN #{@turn} ---------"
+      questioning_player = @players.first
+      answering_player = @players.last
 
-    solution = answering_player.answer(numbers.first, numbers.last)
+      numbers = questioning_player.question
 
-    if (solution == numbers.first + numbers.last) 
-      puts "#{questioning_player.name}: YES! You are correct"
-      answering_player.calculate_score(true)
-    else
-      puts "#{questioning_player.name}: Seriously? No!"
-      answering_player.calculate_score(false)
+      solution = answering_player.answer(numbers.first, numbers.last)
+
+      if (solution == numbers.first + numbers.last) 
+        puts "#{questioning_player.name}: YES! You are correct"
+        answering_player.calculate_score(true)
+      else
+        puts "#{questioning_player.name}: Seriously? No!"
+        answering_player.calculate_score(false)
+      end
+      
+      display_score
+
+      new_turn
+
+      puts
+
     end
     
-    display_score
- 
+    puts "-------- IT'S OVER! ------"
+    puts "The winner is... #{winner.name} with #{winner.remaining} health left!"
+    puts "-------- SEE YA! --------"
   end
 end
