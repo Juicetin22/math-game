33dfd5acb8f67ccdc62d8d5d2eeaccc31d5658e2
Justin Huang <justin.2260@gmail.com>
2022-01-31 23:18:10 -0500

    give access to local variables, create question and answer methods

--- player.rb
+++ player.rb
@@ -1,10 +1,32 @@
 class Player
+  attr_reader :name
+  attr_accessor :score, :remaining
+
   def initialize(name)
     @name = name
     @score = 3
     @remaining = "#{@score}/3"
   end
-end
 
-player_1 = Player.new("Bob");
-p player_1
+  def question
+    print "#{name}! Pick a number from 1 - 20: "
+    number_1 = gets.chomp.to_i
+
+    print "Pick another number from 1 - 20: "
+    number_2 = gets.chomp.to_i
+
+    numbers = [number_1, number_2]
+  end
+
+  def answer(number_1, number_2)
+    puts "#{name}! What does #{number_1} plus #{number_2} equal?"
+    answer = gets.chomp.to_i
+  end
+
+  def calculate_score(correct)
+    if (!correct)
+      @score -= 1
+      @remaining = "#{@score}/3"
+    end
+  end
+end
